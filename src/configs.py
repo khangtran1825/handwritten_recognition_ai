86c4be4860b349d6c4dc99dca98db4d54430bb24
khangtran <tranthaikhang1825@gmail.com>
2025-12-17 18:40:21 +0700

    first model

--- src/configs.py
+++ src/configs.py
@@ -19,7 +19,7 @@ class ModelConfigs(BaseModelConfigs):
         self.height = 96
         self.width = 1408
         self.max_text_length = 0
-        self.batch_size = 32
+        self.batch_size = 16
         self.learning_rate = 0.0005
         self.train_epochs = 1000
         self.train_workers = 20
